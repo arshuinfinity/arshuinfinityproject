c5bcee16843e331f7a339e0033be64131cf95663
BHALAVIGNESH R <balavignesh@millionitechnology.com>
2023-09-08 11:05:51 +0530

    qr code updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,15 @@
-import QRCode from 'qrcode.react';
+import QRCode from "qrcode.react";
+import "./Home.css";
 function Home() {
   return (
-    <div>
-      <h1>QR Code Generator</h1>
-      <QRCode value='https://arshuinfinity.com' />
+    <div className="centered">
+      <h1>ArshuInfinity Website</h1>
+      <QRCode value="http://www.arshuinfinity.com/" />
+      <p>
+        Scan the QR code above or visit the website{" "}
+        <a href="http://www.arshuinfinity.com/">www.arshuinfinity.com</a>
+      </p>
     </div>
   );
-
 }
 export default Home;
